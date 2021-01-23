a6791c5aec03c2c089e64f14f5d5631a7093573f
Mikhayil <58036447+Miskatop@users.noreply.github.com>
2021-01-23 22:16:27 +0400

    New Version for encode and Decode Any string )
    New Formula for encription

--- README.md
+++ README.md
@@ -1,26 +1,13 @@
-# T9 Encoder and Decoder, V-0.3
+# T9 Encoder and Decoder on Python, V-0.2
 #### T9 library allows use T9 keyboard combination to encoding and decoding string
 #### T9 keyboard you can view in pushbutton nokia keyboard (Nokia 3310)
 
 ## Encode/Decode strings
-#### this is exaple of encoded string : `44|31|52|52|62|26|94|62|72|52|30|`
+#### this is exaple of encoded string : `4431525262269062725230`
 #### this is decoden t9 string : `Hello world`
 
-## How to install
-```bash
-sudo python3 setup.py
-```
----
-
 ## How to use
-### JavaScript
-```js
-t9 = new T9(); // initializing T9 class
-let encoded_string = t9.encode('Hello World !!!'); // encoding string
-console.log(encoded_string)
-console.log(t9.decode(encoded_string)) // decoding encoded string
-```
-### Python3
+
 ```python 
 from t9 import T9 # import T9
 
@@ -30,46 +17,6 @@ enc = x.encode('Hello world') # encoding string
 print(f'this is exaple of encoded string : {enc}')
 print(f'this is decoden t9 string : {x.decode(enc)}') # decoding encoded string
 ```
-
-## You can inject key to string
----
-#### Python
-```python
-from t9 import T9 # import T9
-
-x = T9() # initializing T9 class
-string = 'hello world' 
-enc = x.encode('Hello world', key = '123456') # encoding string
-print(f'this is exaple of encoded string : {enc}')
-dec = x.decode(enc, key = '123456')
-print(f'this is decoden t9 string : {dec}') # decoding encoded string
-```
-### For injecting key send encode function 'key' parameter  `
-```js
-t9.encode('Any String', key = 'Your Key')
-```
-
-## Release Info
-> Version-0.1
->> Basic Working code
-
-> Version-0.2
->> Optimize Code And new encodeing type
-
-> Version-0.3
->> Create Setup.py for installing in python
->
->> Cythonize
->
->> Add new chars
-
-> Version-0.4
->> Add javascript encoder
->
->> Add key mode to scripts ( 20.10.2020 - Only python )
-
-> Version-0.4
->> Add key mode to all scripts
 
 [My Facebook](https://www.facebook.com/King.of.the.wold.Misha/)
 
--- t9.py
+++ t9.py
@@ -1,91 +1,117 @@
-#!usr/bin/env python3
+#!/usr/bin/python3
 class T9:
-    """usage type :
-    x = T9()
-    string = 'hello world'
-    enc = x.encode('Hello world')
-    print(f'this is exaple of encoded string : {enc}')
-    print(f'this is decoden t9 string : {x.decode(enc)}')"""
-    structure = {
-        1: ['0', '2', '3', '1', '7', '4', '5', '6', '9', '8', '%', '(', ')'],
-        2: ['a', 'b', 'c', 'A', 'B', 'C', 'sp', '$', '!', '@', '^', '&', '*'],
-        3: ['d', 'e', 'f', 'D', 'E', 'F', '_', '-', '=', '+', '~', '`', '<'],
-        4: ['g', 'h', 'i', 'G', 'H', 'I', '>', '"', "'", ',', '.', '/', '\\'],
-        5: ['j', 'k', 'l', 'J', 'K', 'L', '{', '}', '[', ']', '|', '՞'],
-        6: ['m', 'n', 'o', 'M', 'N', 'O'],
-        7: ['p', 'q', 'r', 's', 'P', 'Q', 'R', 'S'],
-        8: ['t', 'u', 'v', 'T', 'U', 'V'],
-        9: ['w', 'x', 'y', 'z', 'W', 'X', 'Y', 'Z'],
-    }
+	"""usage type :
+	x = T9()
+	string = 'hello world'
+	enc = x.encode('Hello world')
+	print(f'this is exaple of encoded string : {enc}')
+	print(f'this is decoden t9 string : {x.decode(enc)}')"""
+	structure = {
+		1: ['0', '2', '3', '1', '7', '4', '5', '6', '9', '8', '%', '(', ')'],
+		2: ['a', 'b', 'c', 'A', 'B', 'C', 'sp', '$', '!', '@', '^', '&', '*'],
+		3: ['d', 'e', 'f', 'D', 'E', 'F', '_', '-', '=', '+', '~', '`', '<'],
+		4: ['g', 'h', 'i', 'G', 'H', 'I', '>', '"', "'", ',', '.', '/', '\\'],
+		5: ['j', 'k', 'l', 'J', 'K', 'L', '{', '}', '[', ']', '|', '՞'],
+		6: ['m', 'n', 'o', 'M', 'N', 'O'],
+		7: ['p', 'q', 'r', 's', 'P', 'Q', 'R', 'S'],
+		8: ['t', 'u', 'v', 'T', 'U', 'V'],
+		9: ['w', 'x', 'y', 'z', 'W', 'X', 'Y', 'Z'],
+	}
+	unknown = '*'
+	_all_chars = []
 
-    def _encrypt(self, string):
-        res = ''
-        for i in string:
-            for c, v in self.structure.items():
-                if i == ' ':
-                    i = 'sp'
-                if i in v:
-                    res += str(c) + str(v.index(i)) + '|'
-        return res
+	for c in structure.values():
+		_all_chars.extend(c)
 
-    def _decrypt(self, string):
-        res = ''
-        for i in string.split('|'):
-            i = i if len(i) > 1 else False
-            if i:
-                decoded_char = self.structure[int(i[0])][int(i[1:])]
-            if decoded_char == 'sp':
-                decoded_char = ' '
-            res += decoded_char if i else ''
+	def _encrypt(self, string): 
+		res = ''
+		for i in string:
+			for c, v in self.structure.items():
+				if i == ' ':
+					i = 'sp'
 
-        return res
+				if i in v:
+					res += str(c) + str(v.index(i)) + '|'
+				elif not i in v and not i in self._all_chars:
+					res += str(self.unknown) + str(i) + '|'
+					break
+		return res
 
-    def encode(self, string, key=None):
-        encrypted = ''
+	def _decrypt(self, string):
+		res = ''
+		for i in string.split('|'):
+			decoded_char = None
+			i = i if len(i) > 1 else False
+			if i:
+				key = i[0]
+				if key == self.unknown:
+					decoded_char = i[1:]
+				else:
+					decoded_char = self.structure[int(key)][int(i[1:])]
 
-        if key:
-            encrypted+= self._encrypt(key)+'{||}'
-        
-        encrypted+=self._encrypt(string)
-        
-        return encrypted
+			if decoded_char == 'sp':
+				decoded_char = ' '
 
-    def decode(self, string, key=None):
-        if not key and '{||}' in string:
-            raise KeyError('string have a secure key')
+			res += decoded_char if i else ''
 
-        if '{||}' in string:
-            key_hash = string.split('{||}')
-            string = key_hash[1]
-            code = self._decrypt(key_hash[0])
+		return res
 
-            if key != code:
-                raise KeyError('Key is invalid')
+	def encode(self, string, key=None):
+		encrypted = ''
 
-        decrypted = ''
+		if key:
+			encrypted += self._encrypt(key) + '{k}'
 
+		encrypted += self._encrypt(string)
 
-        decrypted+=self._decrypt(string)
+		return encrypted
 
-        return decrypted
+	def decode(self, string, key=None):
+		if not key and '{k}' in string:
+			raise KeyError('string is protected by secure key')
 
+		if '{k}' in string:
+			key_hash = string.split('{k}')
+			string = key_hash[1]
+			code = self._decrypt(key_hash[0])
+
+			if key != code:
+				raise KeyError('Key is invalid')
+
+		decrypted = ''
+
+		decrypted += self._decrypt(string)
+
+		return decrypted
 
 if __name__ == '__main__':
-    import sys
-    import timeit
-    argv = sys.argv
-    t9 = T9()
+	import timeit
+	from argparse import ArgumentParser
 
-    start = timeit.default_timer()
+	t9 = T9()
 
-    if '-e' in argv:
-        print(t9.encode(argv[-1],))
-    elif '-d' in argv:
-        print(t9.decode(argv[-1]))
-    else:
-        enc = t9.encode('Hello World !!!')
-        print(f'this is exaple of encoded string : {enc}')
-        decoded = t9.decode(enc)
-        print(f'this is decoden t9 string : {decoded}')
-    stop = timeit.default_timer()
-    print('Time: ', stop - start)
+	parser = ArgumentParser(description='T9 - encoder & decoder python version')
+
+	parser.add_argument('-e', '--encode', default=None, help='string for encoding.' , type=str)
+	parser.add_argument('-d', '--decode', default=None, help='string for decoding.' , type=str)
+	parser.add_argument('-v', '--demo'  , default="Hello World !!!", help='Demo version of this app.', type=str)
+	
+	result = parser.parse_args()
+
+	if not result.encode and not result.decode and not result.demo :
+		parser.error('Enter the command line arguments')
+
+	if result.encode:
+		print('Encode: \n######## ->\n')
+		print(t9.encode(result.encode))
+
+	if result.decode:
+		print('Decode: \n######## ->\n')
+		print(t9.decode(result.decode))
+
+	if result.demo:
+		print('Demo encode: \n######## ->\n')
+		enc = t9.encode(result.demo)
+		print(enc)
+		print('Demo decode: \n######## ->\n')
+		print(t9.decode(enc))
